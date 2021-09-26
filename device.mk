1293803ad50ce9aca5c68bdbb67652d2593a8e36
rajadeja <rajsjadeja4@gmail.com>
2021-09-26 23:07:31 +0700

    surya: Add authsecret
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- device.mk
+++ device.mk
@@ -78,6 +78,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Authsecret HAL
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
