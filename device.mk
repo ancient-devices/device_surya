3c30f22873330d2d968e07452f8e757e99fd7c61
rezaadi0105 <rezaadipangestu5@gmail.com>
2021-09-29 16:10:32 +0700

    surya: ancient-ify
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- device.mk
+++ device.mk
@@ -319,7 +319,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-ancient
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
